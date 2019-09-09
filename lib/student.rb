d1b699c08fbd15ede69533abbdd50d68f2c4720f
James Scalise <jamescscalise@gmail.com>
2019-09-09 21:30:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,13 @@
 require_relative "./user.rb"
-
+  attr_reader :knowledge
 class Student < User
-
+ def initialize
+   @knowledge = []
+ 
+ 
+ def learn(string)
+   @knowledge << string
+ end
+ 
+ 
 end
